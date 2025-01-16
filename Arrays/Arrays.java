f1498f5f47cca303793ab1018d9d9aff52d2fff3
satishgupta <satishguptagps@gmail.com>
2025-01-16 22:35:41 +0530

    binary Search

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -46,29 +46,95 @@ public class Arrays {
     //     return largest;
     // }
 
-    public static int Binary_Search(int numbers[],int key ){
-        int start=0,end=numbers.length-1;
-        while (start<=end) {
-            int mid=(start+end)/2;
-            // comparisons
-            // found
-            if(numbers[mid]==key){
-                return mid;
+    // public static int Binary_Search(int numbers[],int key ){
+    //     int start=0,end=numbers.length-1;
+    //     while (start<=end) {
+    //         int mid=(start+end)/2;
+    //         // comparisons
+    //         // found
+    //         if(numbers[mid]==key){
+    //             return mid;
+    //         }
+    //         // left
+    //         if(numbers[mid]< key){
+    //             start=mid+1; 
+    //         }else{
+    //             end=mid-1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    // public static void reverse(int numbers[]){
+    //     int first=0,last=numbers.length-1;
+    //     while (first<last) {
+    //         //swap
+    //         int temp= numbers[last];
+    //         numbers[last]= numbers[first];
+    //         numbers[first]=temp;
+
+    //         first++;
+    //         last--;
+            
+    //     }
+    // }
+
+    // Pair Funtion
+    // public static void Pair(int numbers[]){
+    //     int tp=0;
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         int count=(numbers[i]);
+    //         for (int j = i+1; j < numbers.length; j++) {
+    //             System.out.print("(" +count+ ","+numbers[j]+")");
+    //             tp++;
+
+    //         }
+    //         System.out.println();
+    //     }
+    //     System.out.println(tp);
+    // }
+
+    // Sub Arrays
+    public static void printSubArray(int numbers[]){
+        int tp=0;
+        for (int i = 0; i < numbers.length; i++) {
+            int start=i;
+            for (int j = i; j < numbers.length; j++) {
+                int end=j;
+                for (int k = start; k <=end; k++) {
+                    System.out.print(numbers[k]+" ");
+                }
+                tp++;
+                System.out.println();
             }
-            // left
-            if(numbers[mid]< key){
-                start=mid+1; 
-            }else{
-                end=mid-1;
-            }
+            System.out.println();
         }
-        return -1;
+        System.out.println(tp);
     }
+
+
+
+
     public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+          printSubArray(numbers);
+        // Pairs
+        // Pair(numbers);
+
+
+        // reverse a number
+        // int numbers[]={2,4,6,8,10};
+        // System.out.println(numbers.length);
+        // reverse(numbers);
+        // for (int i = 0; i < numbers.length; i++) {
+        //     System.out.print(numbers[i]+ " " );
+        // }
+        // System.out.println();
+        
         // Binary search 
-        int numbers[]={2,4,6,8,10,12,14};
-        int key=10;
-        System.out.println("Index for key is:"+ Binary_Search(numbers,key));
+        // int numbers[]={2,4,6,8,10,12,14};
+        // int key=10;
+        // System.out.println("Index for key is:"+ Binary_Search(numbers,key));
          
          // find the largest number in a given array
         // int numbers[]={1,2,6,3,5};
